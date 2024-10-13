485282f49b0322f55466ec3d00dfe5da81992ca0
Wolvrin12 <anouarchlih31@gmail.com>
2024-10-13 19:01:21 +0100

    Add Header

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,12 @@
+import Header from "../containers/Header";
 import "../styles/App.css";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
 };
 
 export default App;
--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,5 +1,8 @@
+import { useEffect } from "react";
+import "../styles/Header.css";
+
 const Header = () => {
-  return <div>Header</div>;
+  return <div className="Header">For Mounati : </div>;
 };
 
 export default Header;
